4a6fb7ece44ace1da73d0d17a9010c5099f38115
yuasys <2534721+yuasys@users.noreply.github.com>
2022-09-25 08:16:14 +0900

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Hello, World!</h1>
+    </div>
+  )
+}
+
+export default App
